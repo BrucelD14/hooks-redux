cf7e11fe318e9c040b051d80b4cd32afb0993e11
BrucelD14 <samuderaduta144@gmail.com>
2022-11-11 22:50:29 +0700

    init reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, compose, applyMidleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
