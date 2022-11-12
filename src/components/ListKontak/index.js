e30801b8c44b41b388598d12058f4d25692584b6
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 13:35:47 +0700

    show reducer to UI

--- src/components/ListKontak/index.js
+++ src/components/ListKontak/index.js
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getListKontak } from "../../actions/kontakAction";
 
 function ListKontak() {
+  const { getListKontakResult, getListKontakLoading, getListKontakError } =
+    useSelector((state) => state.KontakReducer);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,6 +17,17 @@ function ListKontak() {
   return (
     <div>
       <h4>LIST KONTAK</h4>
+      {getListKontakResult ? (
+        getListKontakResult.map((kontak) => {
+          return(
+            <p key={kontak.id}>{kontak.nama} - {kontak.nohp}</p>
+          )
+        })
+      ) : getListKontakLoading ? (
+        <i>Loading...</i>
+      ) : (
+        <p>{getListKontakError ? getListKontakError : "Data Kosong"}</p>
+      )}
     </div>
   );
 }
