6febe6f74dd0c643489cc932a63642052425e65b
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 13:24:32 +0700

    add reducer

--- src/reducers/kontak/index.js
+++ src/reducers/kontak/index.js
@@ -1,12 +1,23 @@
-const initialState = {
+import { GET_LIST_KONTAK } from "../../actions/kontakAction";
 
-}
+const initialState = {
+  getListKontakResult: false,
+  getListKontakLoading: false,
+  getListKontakError: false,
+};
 
 const kontak = (state = initialState, action) => {
-    switch(action.type) {
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case GET_LIST_KONTAK:
+      return {
+        ...state,
+        getListKontakResult: action.payload.data,
+        getListKontakLoading: action.payload.loading,
+        getListKontakError: action.payload.errorMessage,
+      };
+    default:
+      return state;
+  }
+};
 
 export default kontak;
