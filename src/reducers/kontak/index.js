a0713fa65e13d79f1e925552babb5423fd7f56a6
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 14:36:02 +0700

    add reducer

--- src/reducers/kontak/index.js
+++ src/reducers/kontak/index.js
@@ -1,9 +1,13 @@
-import { GET_LIST_KONTAK } from "../../actions/kontakAction";
+import { GET_LIST_KONTAK, ADD_KONTAK } from "../../actions/kontakAction";
 
 const initialState = {
   getListKontakResult: false,
   getListKontakLoading: false,
   getListKontakError: false,
+
+  addKontakResult: false,
+  addKontakLoading: false,
+  addKontakError: false,
 };
 
 const kontak = (state = initialState, action) => {
@@ -15,6 +19,15 @@ const kontak = (state = initialState, action) => {
         getListKontakResult: action.payload.data,
         getListKontakLoading: action.payload.loading,
         getListKontakError: action.payload.errorMessage,
+      };
+
+    case ADD_KONTAK:
+        console.log("4. Masuk reducer : ", action);
+      return {
+        ...state,
+        addKontakResult: action.payload.data,
+        addKontakLoading: action.payload.loading,
+        addKontakError: action.payload.errorMessage,
       };
     default:
       return state;
