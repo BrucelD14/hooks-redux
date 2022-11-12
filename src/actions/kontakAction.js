ef58a9a65e0aa7f11a888a8819b91b750ddfdb51
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 14:32:35 +0700

    add action ADD_KONTAK

--- src/actions/kontakAction.js
+++ src/actions/kontakAction.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_LIST_KONTAK = "GET_LIST_KONTAK";
+export const ADD_KONTAK = "ADD_KONTAK";
 
 export const getListKontak = () => {
   console.log("2. Masuk action");
@@ -38,6 +39,53 @@ export const getListKontak = () => {
         // gagal get API
         dispatch({
           type: GET_LIST_KONTAK,
+          payload: {
+            loading: false,
+            data: false,
+            errorMessage: error.message,
+          },
+        });
+      });
+  };
+};
+
+export const addKontak = (data) => {
+  console.log("2. Masuk action");
+  return (dispatch) => {
+    // loading
+    dispatch({
+      type: ADD_KONTAK,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false,
+      },
+    });
+
+    //get API
+    axios({
+      method: "POST",
+      url: "http://localhost:3000/kontaks",
+      timeout: 120000,
+      data: data
+    })
+      .then((response) => {
+        console.log('3. berhasil dapat data: ', response.data);
+        // berhasil get API
+        dispatch({
+          type: ADD_KONTAK,
+          payload: {
+            loading: false,
+            data: response.data,
+            errorMessage: false,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log('3. gagal dapat data', error);
+        // gagal get API
+        dispatch({
+          type: ADD_KONTAK,
           payload: {
             loading: false,
             data: false,
