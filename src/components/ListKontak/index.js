098574e906b2cf7c9f32e8899317ccc6af509933
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 12:53:38 +0700

    add action get list kontak

---
+++ src/components/ListKontak/index.js
@@ -0,0 +1,16 @@
+import {useEffect} from 'react';
+
+function ListKontak() {
+
+    useEffect(() => {
+        // get list kontak
+    })
+
+  return (
+    <div>
+      <h4>LIST KONTAK</h4>
+    </div>
+  );
+}
+
+export default ListKontak;
