69a52bd3775d9c089fbbec24e373801e8e81122f
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 17:46:04 +0700

    integrate extenxion redux devTool

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
