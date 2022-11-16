2837678f75d68c15430464567cf2637cf0cd22e0
BrucelD14 <samuderaduta144@gmail.com>
2022-11-16 14:20:48 +0700

    add actions DELETE_KONTAK

--- src/actions/kontakAction.js
+++ src/actions/kontakAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_LIST_KONTAK = "GET_LIST_KONTAK";
 export const ADD_KONTAK = "ADD_KONTAK";
+export const DELETE_KONTAK = "DELETE_KONTAK";
 
 export const getListKontak = () => {
   return (dispatch) => {
@@ -80,6 +81,49 @@ export const addKontak = (data) => {
         // gagal get API
         dispatch({
           type: ADD_KONTAK,
+          payload: {
+            loading: false,
+            data: false,
+            errorMessage: error.message,
+          },
+        });
+      });
+  };
+};
+
+export const deleteKontak = (id) => {
+  return (dispatch) => {
+    // loading
+    dispatch({
+      type: DELETE_KONTAK,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false,
+      },
+    });
+
+    //get API
+    axios({
+      method: "DELETE",
+      url: `http://localhost:3000/kontaks/${id}`,
+      timeout: 120000,
+    })
+      .then((response) => {
+        // berhasil get API
+        dispatch({
+          type: DELETE_KONTAK,
+          payload: {
+            loading: false,
+            data: response.data,
+            errorMessage: false,
+          },
+        });
+      })
+      .catch((error) => {
+        // gagal get API
+        dispatch({
+          type: DELETE_KONTAK,
           payload: {
             loading: false,
             data: false,
